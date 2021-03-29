2176cee4083df3b3f4d11cf4d6e47a06bf86cd08
Damian Barrios <barrios.d122@gmail.com>
2021-03-29 16:07:58 -0300

    added swipe component

---
+++ components/Swipes.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import Swipeable from 'react-native-gesture-handler/Swipeable'
+import {RectButton} from 'react-native-gesture-handler'
+import SwipeableImage from './SwipeableImage'
+
+export default function Swipes({users, currentIndex}) {
+    
+    const renderLeftActions = () => {
+        return (
+        <RectButton style={styles.container}>
+            <SwipeableImage user={users[currentIndex + 1]}></SwipeableImage>
+        </RectButton>   
+        )
+    }
+    const renderRightActions = () => {
+        return (
+        <RectButton style={styles.container}>
+            <SwipeableImage user={users[currentIndex + 1]}></SwipeableImage>
+        </RectButton>
+        )
+    }
+
+    return (
+        <Swipeable 
+            friction={2}
+            leftThreshold={40}
+            rightThreshold={40}
+            renderLeftActions={renderLeftActions}
+            renderRightActions={renderRightActions}
+        >
+            <SwipeableImage user={users[currentIndex]}>
+
+            </SwipeableImage>
+        </Swipeable>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+
+
+})
