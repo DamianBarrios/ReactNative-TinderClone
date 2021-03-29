bf94282a1960f639cfc8830680338c5e7c0b5e77
Damian Barrios <barrios.d122@gmail.com>
2021-03-29 16:42:38 -0300

    added box for like and pass in photos

--- components/SwipeableImage.js
+++ components/SwipeableImage.js
@@ -2,10 +2,20 @@ import React from 'react';
 import { View, Image, StyleSheet, Text } from 'react-native';
 import {FontAwesome} from '@expo/vector-icons';
 
-export default function SwipeableImage( {user} ) {
+export default function SwipeableImage( {user , willLike, willPass} ) {
     return (
         <View>
             <Image source= {{uri: user.picture.large}} style={styles.photo}/>
+            {willLike && (
+                    <View style={styles.likeBox}>
+                        <Text style={{...styles.textPrimary, color:'#64EDCC'}}>LIKE</Text>
+                    </View>
+            )}
+            {willPass && (
+                    <View style={styles.passBox}>
+                        <Text style={{...styles.textPrimary, color:'#F06795'}}>PASS</Text>
+                    </View>
+            )}
             <View style={styles.textContainer}>
                 <View style={styles.textRow}>
                     <Text style={[styles.textPrimary, styles.textShadow]}>{user.name.first}</Text>
@@ -20,9 +30,31 @@ export default function SwipeableImage( {user} ) {
     )
 }
 
+const boxStyle = {
+    position: 'absolute',
+    top: '50%',
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    borderWidth: 3,
+    borderRadius: 10
+}
+
 const styles = StyleSheet.create({
+    
+    likeBox:{
+        ...boxStyle,
+        left: 40,
+        borderColor: '#64EDCC'
+    },
+    passBox:{
+        ...boxStyle,
+        right: 40,
+        borderColor: '#F06795'
+    },
     photo: {
-        height: '100%',
+        height: '90%',
         resizeMode: 'cover',
         borderRadius: 40,
     },
